7e78c1eb474b89dcb581f878201b55d64b850419
Micah Lund <micahlund@micah-lunds-macbook-pro.local>
2019-01-19 09:32:32 -0700

    project started for my first React app after finishing the Udemy course

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+/*
+
+function Person(props) {
+  return (
+    <div className="person">
+      <h1>{props.name}</h1>
+      <p>Your Age: {props.age}</p>
+    </div>
+  );
+}
+
+var app = (
+  <div>
+    <Person name="Gojira" age="9" />
+    <Person name="Kaiju" age="3" />
+  </div>
+);
+
+ReactDOM.render(app, document.querySelector("#app"));
+
+*/
